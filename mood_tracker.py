71cda19d5d8b37a344fa2af2762428e28b5daf84
Emma-Downie <15163@coderacademy.edu.au>
2023-12-21 21:05:32 +1100

    Try / Except added

--- mood_tracker.py
+++ mood_tracker.py
@@ -1,3 +1,24 @@
+from mood_tracker_functions import add_entry, add_emotions, add_score, remove_entry, view_entries
+
+file_name = "entries.csv"
+
+try:
+    #open the file in read mode
+    entries_file = open(file_name, "r")
+    entries_file.close
+    print("In try block")
+    #if it rhows error, it means the file doesn't exist
+    #if no error, it mesns the file exists
+except:
+    #now we knhow the file doesnt exist
+    #create the file
+    entries_file = open(file_name, "w")
+    #we can also insert the first line into the file
+    entries_file.write("title,completed")
+    entries_file.close()
+    print("In except block")
+
+
 print("Welcome to your Mood Tracker")
 
 def create_menu():
@@ -15,15 +36,15 @@ users_choice = ""
 while users_choice != "6":
     users_choice = create_menu()
     if (users_choice == "1"):
-        print("You entered 1")
+        add_entry()
     elif (users_choice == "2"):
-        print("You enetered 2")
+        remove_entry()
     elif(users_choice == "3"):
-        print("You entered 3")
+        add_score()
     elif (users_choice == "4"):
-        print("You enetered 4")
+        add_emotions()
     elif(users_choice == "5"):
-        print("You entered 5")
+        view_entries()
     elif(users_choice == "6"):
         continue
     else:
---
+++ mood_tracker_functions.py
@@ -0,0 +1,14 @@
+def add_entry():
+    print("Add Entry")
+
+def remove_entry():
+    print("Remove Entry")
+
+def add_score():
+    print("Rate day / 10")
+
+def add_emotions():
+    print("Add key emotions")
+
+def view_entries():
+    print("View all entries")
