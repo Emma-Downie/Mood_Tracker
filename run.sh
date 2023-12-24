5744252a84c6eea14f7daf8147da4809fcb7f63d
Emma-Downie <15163@coderacademy.edu.au>
2023-12-24 20:33:08 +1030

    Issue fixed in venv within run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # check if python is installed 
-    #print on screen please instal python if not installed
 
-python3 -m venv .mood_venv
+python3 -m venv .venv
 source .venv/bin/activate
 pip3 install colored
 pip3 install pytest
